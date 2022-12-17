8224e3541304b1af729c68d3457e3532235c3eaa
cerasusflorere <cerasusflorere.180@gmail.com>
2022-12-17 10:42:36 +0900

    64-小道具リスト並び

--- resources/js/components/List_Props.vue
+++ resources/js/components/List_Props.vue
@@ -52,6 +52,8 @@ export default {
         if(this.postFlag){
           const props = await this.fetchProps();
 
+          this.sort_Standard();
+
           const content_dom = this.$refs.content_list_props;
           const content_rect = content_dom.getBoundingClientRect(); // 要素の座標と幅と高さを取得
   
@@ -76,6 +78,67 @@ export default {
       }
       
       this.props_list = response.data;
+    },
+
+    
+
+    sort_Standard(){
+      const regex_str = /[^ぁ-んー]/g; // ひらがな以外
+      const regex_number = /[^0-9]/g; // 数字以外
+      const regex_alf = /[^A-Z]/g; // アルファベット
+      this.props_list.sort((a, b) => {
+        // kanaで並び替え
+        if(a.kana !== b.kana){
+          const a_str = a.kana.replace(regex_str, "");
+          const b_str = b.kana.replace(regex_str, "");
+          let a_number = a.kana.replace(regex_number, "");
+          let b_number = b.kana.replace(regex_number, "");
+          const a_alf = a.kana.replace(regex_alf, "");
+          const b_alf = b.kana.replace(regex_alf, "");
+
+          if(a_str !== b_str){
+            let sort_str = a_str;
+            if([...b_str].length < [...a_str].length){
+              sort_str = b_str;
+            } 
+            for(let i=0; i < [...sort_str].length; i++){
+              if(a_str.codePointAt(i) !== b_str.codePointAt(i)){
+                if(a_str.codePointAt(i) > b_str.codePointAt(i)){
+                  return 1;
+                }else if(a_str.codePointAt(i) < b_str.codePointAt(i)){
+                  return -1;
+                }
+              }          
+            }
+          }
+
+          if(a_number !== b_number){
+            if(!a_number){
+              a_number = 0;
+            }
+            if(!b_number){
+              b_number = 0;
+            }
+
+            if(parseInt(a_number) > parseInt(b_number)){
+              return 1;
+            }else if(parseInt(a_number) < parseInt(b_number)){
+              return -1;
+            }
+          }
+
+          if(a_alf !== b_alf){
+            if(a_alf.codePointAt(0) > b_alf.codePointAt(0)){
+              return 1;
+            }else if(a_alf.codePointAt(0) < b_alf.codePointAt(0)){
+              return -1;
+            }else{
+              return 0;
+            }
+          }
+        }
+        return 0;
+      });
     },
 
     // 小道具詳細のモーダル表示 
